db243768f05658e7f6f57336c81ab48a465d8c1c
jrtm96 <jrtm96@icloud.com>
2024-01-18 12:38:13 -0600

    Implemented Middleware, successful GET request handled

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+    const { method, path } = req;
+    console.log(
+        `New request to: ${method} ${path} at ${new Date().toISOString()}`
+    );
+    next();
+});
+
 const publicDirectoryPath = path.join(__dirname, './public');
 app.use(express.static(publicDirectoryPath));
 
