b405f2b357ef07ca5d7f5b05d79028d97b2bd0e4
jrtm96 <jrtm96@icloud.com>
2024-01-18 12:20:45 -0600

    begin working on express app and nodemon

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, './public');
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", (req, res) => {
+    res.send("Hello Jose, welcome! or whatever");
+});
+
+app.get("/:name", (req, res) => {
+    res.send(`Welcome to Express Recipes, ${req.params.name}!`);   
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server is up on port: ${port}.`);
+});
